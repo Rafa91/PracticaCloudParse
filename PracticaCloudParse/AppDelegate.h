622bf4b6ff2c67a48d843248ef4d1e54c37ac2bf
Rafael Navarro <Rafa@MacBook-Pro-de-Rafael.local>
2015-04-30 10:15:58 +0200

    Initial Commit

---
+++ PracticaCloudParse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticaCloudParse
+//
+//  Created by Rafael Navarro on 30/4/15.
+//  Copyright (c) 2015 Rafael Navarro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PracticaCloudParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticaCloudParse
+//
+//  Created by Rafael Navarro on 30/4/15.
+//  Copyright (c) 2015 Rafael Navarro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
